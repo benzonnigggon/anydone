278ccc555f9dffb72fc25736dbf21ee047fe5f6e
mrtrooper <67683294+mrtrooper@users.noreply.github.com>
2022-06-16 15:58:09 +0545

    Nhugu_File

---
+++ careerDetails.php
@@ -0,0 +1,78 @@
+<?php
+    include 'include/nav.php';
+?>
+
+<main class="main-box">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+            <div class="main-content">
+                <h1>React Native Developer</h1>
+                <p>Application Deadline: 7th May, 2022</p>
+            </div>
+            </div>
+        </div>
+    </div>
+</main>
+
+<section class="job-desc">
+    <div class="container">
+        <div class="desc-list">
+            <p>We are looking for a well-rounded, experienced react developer who specializes in creating high-quality react apps utilizing redux techniques and has at least some experience.</p>
+            <div class="desc-box">
+                <h3>JOB RESPONSIBILITIES</h3>
+                <ul>
+                    <li>Building reusable components and front-end libraries for future use.</li>
+                    <li>Translating designs and wireframes into high-quality code.</li>
+                    <li>Optimizing components for maximum performance across a vast array of web-capable devices and browsers.</li>
+                </ul>
+            </div>
+            <div class="desc-box">
+                <h3>REQUIREMENT AND QUALIFICATIONS</h3>
+                <ul>
+                    <li>1 year of REACT development experience (internship or professional)</li>
+                    <li>Basic experience with HTML and CSS.</li>
+                    <li>Willingness to do pair-programming.</li>
+                    <li>Understanding and Working knowledge of Git.</li>
+                    <li>Understanding of React fundamentals.</li>
+                    <li>Front End development experience using JavaScript (ES2015+), HTML5, and CSS.</li>
+                    <li>Redux.</li>
+                    <li>Experience working and integrating front end applications with RESTful Middle-Tier Applications.</li>
+                </ul>
+            </div>
+            <div class="desc-box">
+                <h3>PREFERABLE</h3>
+                <ul>
+                    <li>Experience with test driven development.</li>
+                    <li>RESTful Web Services experience.</li>
+                    <li>Performance testing.</li>
+                    <li>Knowledge of Git</li>
+                    <li>JIRA or similar agile tools.</li>
+                    <li>Unit testing</li>
+                    <li>Total comfort working in a Linux environment.</li>
+                </ul>
+            </div>
+            <div class="desc-box">
+                <h3>BENEFITS</h3>
+                <ul>
+                    <li>5 working days</li>
+                    <li>Insurance Plan</li>
+                    <li>Citizen Investment Trust (CIT)</li>
+                    <li>Lunch facilities</li>
+                    <li>Attractive salary for deserving candidates</li>
+                    <li>Periodic Salary review and Grading</li>
+                    <li>Periodic refreshment programs</li>
+                </ul>
+            </div>
+            <h3 class="txt-green">APPLY FOR THIS JOB</h3>
+            <p>Please send your CV at <a href="">jobs@anydone.com</a></p>
+        </div>
+    </div>
+    <div class="about-footer">
+        <h1>Maximize productivity within an automated <br> and agile enviroment </h1>
+        <div class="btnplace"><button class='txt-center'>Try Now</button></div>
+    </div>
+</section>
+<?php
+    include 'include/footer.php';
+?>
